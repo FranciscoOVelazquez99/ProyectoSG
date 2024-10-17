359436e9dec4fff24b7aa6413e42dfe0e88fb700
FranciscoOVelazquez99 <olmedoagostinramon@gmail.com>
2024-10-17 11:58:23 -0300

    Styles-Interval-tablename

--- instance/del.py
+++ instance/del.py
@@ -5,7 +5,7 @@ conn = sqlite3.connect('instance/database.db')
 cursor = conn.cursor()
 
 
-cursor.execute(f"DROP TABLE IF EXISTS elements")
+cursor.execute(f"DROP TABLE IF EXISTS maintenance_elm")
 
 conn.commit()
 conn.close()

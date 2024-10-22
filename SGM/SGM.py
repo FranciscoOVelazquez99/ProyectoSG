16058ed97a594848a94de7f6baa18cad2855ff92
FranciscoOVelazquez99 <olmedoagostinramon@gmail.com>
2024-10-22 08:30:48 -0300

    waitress

--- SGM/SGM.py
+++ SGM/SGM.py
@@ -703,5 +703,8 @@ def update_order():
 ############################ ////////////// ###################################################
 
 
-if __name__ == '__main__':
-    app.run()
+from waitress import serve
+
+if __name__ == "__main__":
+    # Iniciar el servidor usando Waitress
+    serve(app, host='0.0.0.0', port=5010)
